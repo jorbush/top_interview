a260d6dd359f60589131b9915ca4f27a653f6eac
Jordi <55429631+jorbush@users.noreply.github.com>
2023-10-19 08:08:25 +0200

    169

---
+++ 169.py
@@ -0,0 +1,32 @@
+"""
+
+"""
+
+
+def majorityElement(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    return majority_element(nums, dict())
+
+
+def majority_element(nums, dictionary):
+    """
+    :type nums: List[int]
+    :type dictionary: dict
+    :rtype: int
+    """
+    if len(nums) == 0:
+        return max(dictionary, key=dictionary.get)
+    current = nums.pop(0)
+    dictionary.get(current)
+    if dictionary.get(current) is None: dictionary[current] = 1
+    else: dictionary[current] += 1
+    print(dictionary)
+    return majority_element(nums, dictionary)
+
+
+if __name__ == "__main__":
+    nums = [3, 2, 3]
+    print(majorityElement(nums))
